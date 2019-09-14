2ccfd4e542c78982d73744508024941b5d362445
Daniel Peach <dpeaches96@gmail.com>
2019-09-14 12:16:06 -0400

    fixed types, need to make arrays for players on games

--- node-server/graphql/types/index.js
+++ node-server/graphql/types/index.js
@@ -1,2 +1,61 @@
-module.exports.ProfileType = require('./Profile')
-module.exports.GameType = require('./Game')
+const {GameModel, GameInstanceModel, ProfileModel} = require('../../mongodb')
+const {GraphQLBoolean, GraphQLID, GraphQLString, GraphQLList, GraphQLInt, GraphQLObjectType} = require('graphql')
+
+const ProfileType = new GraphQLObjectType({
+  name: 'Profile',
+  fields: () => {
+    return {
+      _id: { type: GraphQLID },
+      firstName: { type: GraphQLString },
+      lastName: { type: GraphQLString },
+      accountType: { type: GraphQLString },
+      phoneNumber: { type: GraphQLString },
+      isNumberVerified: { type: GraphQLBoolean },
+      email: { type: GraphQLString },
+      topGames: { type: GraphQLList(GraphQLString)},
+      communityRadius: { type: GraphQLInt },
+      games: {
+        type: new GraphQLList(GameInstanceType),
+        resolve(parent, args) {
+          return GameInstanceModel.find({player1Id: parent._id})
+        }
+      }
+    }
+  }
+})
+
+const GameType = new GraphQLObjectType({
+  name: 'Game',
+  fields: () => {
+    return {
+      id: { type: GraphQLID },
+      title: { type: GraphQLString },
+      timesPlayed: { type: GraphQLInt },
+    }
+  }
+})
+
+const GameInstanceType = new GraphQLObjectType({
+  name: 'GameInstance',
+  fields: () => {
+    return {
+      id: { type: GraphQLID },
+      game: {
+        type: GameType,
+        resolve(parent, args) {
+          return GameModel.findById(parent.gameId)
+        }
+      },
+      player1: {
+        type: ProfileType,
+        resolve(parent, args) {
+          return ProfileModel.findById(parent.player1Id)
+        }
+      }
+    }
+  }
+})
+
+module.exports = {
+  ProfileType, GameType, GameInstanceType
+}
